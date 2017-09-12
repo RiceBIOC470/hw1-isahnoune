383f10a1a2f50caef1bdfe00f29df82a21b78623
isahnoune <31284645+isahnoune@users.noreply.github.com>
2017-09-12 12:28:18 -0500

    Add files via upload

--- HW1.m
+++ HW1.m
@@ -8,10 +8,22 @@
 
 %your code should work no matter which of these lines is uncommented. 
 x = 3; y = 5; % integers
-%x = '3'; y= '5'; %strings
-% x = 3; y = '5'; %mixed
+
+x = '3'; y= '5'; %strings
+
+%x = 3; y = '5'; %mixed
 
 %your code goes here
+
+if ischar(x)
+    x = str2num(x)
+end
+if ischar(y)
+    y = str2num(y)
+end
+
+z = x + y
+
 
 %output your answer
 
@@ -26,25 +38,140 @@ x = 3; y = 5; % integers
 
 N = 500; % define sequence length
 
+sequence = "";
+rand_seq = randi([1 4],[1,N]);
 
-
+for i = 1:N
+    if rand_seq(i) == 1
+        sequence = sequence + "T";
+    end
+    if rand_seq(i) == 2
+        sequence = sequence + "A";
+    end
+    if rand_seq(i) == 3
+        sequence = sequence + "C";
+    end
+    if rand_seq(i) == 4
+        sequence = sequence + "G";
+    end
+end
 %part 2: open reading frames (ORFs) are pieces of DNA that can be
 % transcribed and translated. They start with a start codon (ATG) and end with a
 % stop codon (TAA, TGA, or TAG). Write a piece of code that finds the longest ORF 
 % in your seqeunce rand_seq. Hint: see the function strfind.
 
+startcodon = strfind(sequence, 'ATG');
+stopcodon1 = strfind(sequence, 'TAA');
+stopcodon2 = strfind(sequence, 'TGA');
+stopcodon3 = strfind(sequence, 'TAG');
+
+x = cat(2, stopcodon1,stopcodon2,stopcodon3);
+
+for i = 1:length(startcodon)
+    length_orf(i) = min(x(startcodon(i) < x)) - startcodon(i);
+end
+
 %part 3: copy your code in parts 1 and 2 but place it inside a loop that
 % runs 1000 times. Use this to determine the probability
 % that an sequence of length 500 has an ORF of greater than 50 b.p.
+
+count = 0;
+N = 500; % define sequence length
+for k = 1:1000
+sequence = "";
+rand_seq = randi([1 4],[1,N]);
+    for i = 1:N
+        if rand_seq(i) == 1
+            sequence = sequence + "T";
+        end
+        if rand_seq(i) == 2
+            sequence = sequence + "A";
+        end
+        if rand_seq(i) == 3
+            sequence = sequence + "C";
+        end
+        if rand_seq(i) == 4
+            sequence = sequence + "G";
+        end
+    end
+   
+    startcodon = strfind(sequence, "ATG");
+    stopcodon1 = strfind(sequence, "TAA");
+    stopcodon2 = strfind(sequence, "TGA");
+    stopcodon3 = strfind(sequence, "TAG");
+
+    x = cat(2, stopcodon1,stopcodon2,stopcodon3);
+
+    for i = 1:length(startcodon)
+        if min(x(startcodon(i) < x)) ~= 0
+            length_orf(i) = (min(x(startcodon(i) < x)) - startcodon(i));
+            if max(length_orf) > 50
+                count = count + 1;
+            end
+        end
+    end
+   
+end
+probability = (count/1000);
+
 
 %part 4: copy your code from part 3 but put it inside yet another loop,
 % this time over the sequence length N. Plot the probability of having an
 % ORF > 50 b.p. as a funciton of the sequence length. 
 
+
+sequence_length = 500; % define sequence length
+
+for N = 1:length(sequence_length)
+count = 0;
+N = 500; % define sequence length
+for k = 1:1000
+sequence = "";
+rand_seq = randi([1 4],[1,N]);
+    for i = 1:N
+        if rand_seq(i) == 1
+            sequence = sequence + "T";
+        end
+        if rand_seq(i) == 2
+            sequence = sequence + "A";
+        end
+        if rand_seq(i) == 3
+            sequence = sequence + "C";
+        end
+        if rand_seq(i) == 4
+            sequence = sequence + "G";
+        end
+    end
+   
+    startcodon = strfind(sequence, "ATG");
+    stopcodon1 = strfind(sequence, "TAA");
+    stopcodon2 = strfind(sequence, "TGA");
+    stopcodon3 = strfind(sequence, "TAG");
+
+    x = cat(2, stopcodon1,stopcodon2,stopcodon3);
+
+    for i = 1:length(startcodon)
+        if min(x(startcodon(i) < x)) ~= 0
+            length_orf(i) = (min(x(startcodon(i) < x)) - startcodon(i));
+            if max(length_orf) > 50
+                count = count + 1;
+            end
+        end
+    end
+end
+probability(N) = (count/1000);
+end
+
+figure;
+plot(probability);
+
+
 %part 5: Make sure your results from part 4 are sensible. What features
 % must this curve have (hint: what should be the value when N is small or when
 % N is very large? how should the curve change in between?) Make sure your
 % plot looks like this. 
+
+When the N is small, there shoul be a lower probability that there are ORFs that are greater than 50bps vs. when there is a higher N where that will increase.
 
 %% problem 3 data input/output and simple analysis
 
@@ -60,9 +187,17 @@ N = 500; % define sequence length
 % part1: write code to read the Cp data from this file into a vector. You can ignore the last two
 % rows with positions beginning with G and H as there were no samples here. 
 
+T = readtable('qPCRdata.txt');
+
+T1 = T(1:72, 5:5);
+
+T2 = table2array(T1);
+
 % Part 2: transform this vector into an array representing the layout of
 % the plate. e.g. a 6 row, 12 column array should that data(1,1) = Cp from
 % A1, data(1,2) = Cp from A2, data(2,1) = Cp from B1 etc. 
+% 
+B = reshape (T2, [6,12])
 
 % Part 3. The 4th gene in columns 10 - 12 is known as a normalization gene.
 % That is, it's should not change between conditions and it is used to normalize 
@@ -73,6 +208,7 @@ N = 500; % define sequence length
 % the gene in the 1st condition, CpX is the value of Cp in condition X and
 % CpN0 and CpNX are the same quantitites for the normalization gene.
 % Plot this data in an appropriate way. 
+
 
 
 %% Challenge problems that extend the above (optional)
